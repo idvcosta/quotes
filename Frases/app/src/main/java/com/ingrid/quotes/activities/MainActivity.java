ba8d1be2ed2b4e3ab7504293db540fc47d68da69
Ingrid <ingrid.danielle@gmail.com>
2021-01-05 08:24:41 -0300

    Implemented navigation from main to quotes and authors screens

--- Frases/app/src/main/java/com/ingrid/quotes/activities/MainActivity.java
+++ Frases/app/src/main/java/com/ingrid/quotes/activities/MainActivity.java
@@ -2,7 +2,10 @@ package com.ingrid.quotes.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.ingrid.quotes.R;
 
@@ -12,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btAuthors).setOnClickListener(button -> {
+            onAuthorsClicked();
+        });
+        findViewById(R.id.btQuotes).setOnClickListener(button -> {
+            onQuotesClicked();
+        });
+    }
+
+    private void onAuthorsClicked() {
+        startActivity(new Intent(this, AuthorsActivity.class));
+    }
+
+    private void onQuotesClicked() {
+        startActivity(new Intent(this, QuotesActivity.class));
     }
 }
