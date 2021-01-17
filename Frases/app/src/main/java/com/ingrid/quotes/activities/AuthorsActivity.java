d71ff9b11915123db114881efd6874e418bfd1ea
Ingrid <ingrid.danielle@gmail.com>
2021-01-17 18:25:32 -0300

    Implemented floating button for screen author

--- Frases/app/src/main/java/com/ingrid/quotes/activities/AuthorsActivity.java
+++ Frases/app/src/main/java/com/ingrid/quotes/activities/AuthorsActivity.java
@@ -1,16 +1,21 @@
 package com.ingrid.quotes.activities;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ingrid.quotes.R;
 import com.ingrid.quotes.adapters.AuthorsAdapter;
 import com.ingrid.quotes.model.Author;
@@ -48,15 +53,28 @@ public class AuthorsActivity extends AppCompatActivity {
     }
 
     private void initRegistry() {
-        EditText etAuthor = findViewById(R.id.etAuthor);
-        Button btAdd = findViewById(R.id.btAdd);
-        btAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String authorName = etAuthor.getText().toString();
-                etAuthor.setText("");
-                viewModel.addAuthor(authorName);
-            }
+        FloatingActionButton fabAdd = findViewById(R.id.fabAdd);
+        fabAdd.setOnClickListener(v -> {
+            showRegistryDialog();
         });
+    }
+
+    private void showRegistryDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.dialog_add_author, null);
+        EditText etName = view.findViewById(R.id.etName);
+
+        builder
+                .setTitle(R.string.add_author_title)
+                .setView(view)
+                .setPositiveButton(R.string.bt_add, (dialog, id) -> {
+                    String authorName = etName.getText().toString();
+                    viewModel.addAuthor(authorName);
+                })
+                .setNegativeButton(R.string.bt_cancel, (dialog, id) -> {
+
+                });
+        builder.create().show();
     }
 }
